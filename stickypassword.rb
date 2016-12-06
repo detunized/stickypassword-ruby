081751f2c7d92fbc1ff800922906a6c5075bb285
Dmitry Yakimenko <detunized@gmail.com>
2016-12-06 15:10:03 +0100

    Http class

--- stickypassword.rb
+++ stickypassword.rb
@@ -3,4 +3,38 @@
 require "yaml"
 require "httparty"
 
+# Set to true to redirect everything but S3 to a local mitmproxy instance
+USE_PROXY = false
+
+class Http
+    include HTTParty
+
+    def initialize
+        @options = {
+            verify: false
+        }
+
+        if USE_PROXY
+            @options[:http_proxyaddr] = "localhost"
+            @options[:http_proxyport] = 9999
+        end
+
+        @post_headers = {
+        }
+    end
+
+    def get url, headers = {}
+        self.class.get url, @options.merge({
+            headers: headers
+        })
+    end
+
+    def post url, args, headers = {}
+        self.class.post url, @options.merge({
+            body: args,
+            headers: @post_headers.merge(headers)
+        })
+    end
+end
+
 config = YAML::load_file "config.yaml"
