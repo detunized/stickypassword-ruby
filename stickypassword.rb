a069eb8927498f887f3daea76784a73488b6ea20
Dmitry Yakimenko <detunized@gmail.com>
2016-12-06 18:40:00 +0100

    Request encrypted token

--- stickypassword.rb
+++ stickypassword.rb
@@ -1,7 +1,10 @@
 #!/usr/bin/env ruby
 
 require "yaml"
+require "base64"
 require "httparty"
+
+API_URL = "https://spcb.stickypassword.com/SPCClient"
 
 # Set to true to redirect everything but S3 to a local mitmproxy instance
 USE_PROXY = false
@@ -37,4 +40,43 @@ class Http
     end
 end
 
+# Base64 encoding
+class String
+    def to_64
+        Base64.strict_encode64 self
+    end
+
+    def from_64
+        Base64.strict_decode64 self
+    end
+end
+
+def request_headers device_id
+    {
+        "User-Agent" => "SP/8.0.3436 Prot=2 ID=#{device_id} Lng=EN Os=Android/4.4.4 Lic= LicStat= PackageID=",
+        "Date" => "#{Time.now.httpdate}",
+        "Accept" => "application/xml",
+        "Pragma" => "no-cache",
+        "Cache-Control" => "no-cache",
+        "Content-Type" => "application/x-www-form-urlencoded; charset=UTF-8",
+        "host" => "spcb.stickypassword.com",
+        "Connection" => "Keep-Alive",
+        "Accept-Encoding" => "gzip",
+    }
+end
+
+def get_encrypted_token username, device_id, http
+    response = http.post "#{API_URL}/GetCrpToken", {uaid: username}, request_headers(device_id)
+
+    # TODO: Check for format errors
+    response.parsed_response["SpcResponse"]["GetCrpTokenResponse"]["CrpToken"].from_64
+end
+
+#
+# main
+#
+
 config = YAML::load_file "config.yaml"
+
+http = Http.new
+encrypted_token = get_encrypted_token config["username"], config["device_id"], http
